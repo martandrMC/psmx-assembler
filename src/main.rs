62bcdd8975509f9a06a023ba4164a672ac514a33
Martin Andronikos <martin.andronikos@gmail.com>
2023-07-22 22:51:11 +0300

    Setup LALRPOP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#[macro_use]
+extern crate lalrpop_util;
+lalrpop_mod!(psmx);
+
 fn main() {
-    println!("Hello, world!");
+	let parser = psmx::ThingParser::new();
+	let result = parser.parse("This string is 34 characters long!");
+	println!("{:?}", result);
 }
